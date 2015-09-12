401ba1c72c566862cadd243a334c7e329d79ace6
Xoey Zhang <mengxizh@gmail.com>
2015-09-12 09:58:37 -0400

    New setup + storyboard, logos and buttons

---
+++ Generos/TakeStyle/TakeStyle/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TakeStyle
+//
+//  Created by Mengxi Zhang on 2015-09-12.
+//  Copyright © 2015 MXZ Designs. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
